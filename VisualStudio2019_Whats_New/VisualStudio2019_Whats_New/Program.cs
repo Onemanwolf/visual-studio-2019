db4f0e9c54b845d8b9e27b72b084daa60ce860d1
Tim Oleson <Onemanwolf@users.noreply.github.com>
2019-10-23 15:58:18 -0400

    Updated project

--- VisualStudio2019_Whats_New/VisualStudio2019_Whats_New/Program.cs
+++ VisualStudio2019_Whats_New/VisualStudio2019_Whats_New/Program.cs
@@ -6,7 +6,23 @@ namespace VisualStudio2019_Whats_New
     {
         static void Main(string[] args)
         {
+            
+            
             Console.WriteLine("Hello World!");
+           
+            
+            //error for debug example
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.ReadKey();
+            //error       
+
+         
+
         }
+
+
     }
 }
